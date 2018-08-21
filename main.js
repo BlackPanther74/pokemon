1ac5a749f4cdc93d577e2c139ca50beb5a426a80
Garth Puckerin <garth.puckerin@gmail.com>
2018-08-20 22:59:22 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,29 @@
+axios.get("http://fizal.me/pokeapi/api/26.json")
+.then(catchem => {
+  let poke1 = catchem;
+  M.AutoInit();
+});
+
+
+
+
+
+
+
+// class Trainer {
+//   constructor(name, pokemon) {
+//   this.name = name
+//   this.pokemon = pokemon
+//   }
+// }
+
+// let garth = new Trainer(name, pokemon)
+
+// class Pokemon {
+//   constructor() {
+//   this.name = name
+//   this.weight = weight
+//   this.height = height
+//   }
+// }
+
